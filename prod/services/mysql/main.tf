4da180e2330985fd83d5a6ce0b6faaa86fe2d7f6
Daniil Digtyar <daniildigtyar@gmail.com>
2024-07-29 17:00:45 +0200

    Prod conf and output messages

---
+++ prod/services/mysql/main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+resource "aws_db_instance" "example" {
+  identifier_prefix   = "terraform-up-and-running"
+  engine              = "mysql"
+  allocated_storage   = 10
+  instance_class      = "db.t3.micro"
+  skip_final_snapshot = true
+  db_name             = "prod_database"
+
+  username = var.db_username
+  password = var.db_password
+}
+
+terraform {
+    backend "s3" {
+      # Replace this with your bucket name!
+      bucket = "terraform-dani-bucket"
+      key    = "prod/data-stores/mysql/terraform.tfstate"
+      region = "eu-west-1"
+      # Replace this with your DynamoDB table name!
+      dynamodb_table = "terraform-up-and-running-locks"
+      encrypt        = true
+    }
+  }
+  
---
+++ prod/services/webserver-cluster/main.tf
@@ -0,0 +1,44 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+module "webserver_cluster" {
+  source = "../../../modules/services/webserver-cluster"
+
+  cluster_name           = "webservers-prod"
+  db_remote_state_bucket = "terraform-dani-bucket"
+  db_remote_state_key    = "prod/data-stores/mysql/terraform.tfstate"
+  instance_type          = "m4.large"
+  min_size               = 2
+  max_size               = 10
+}
+
+resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
+  autoscaling_group_name = module.webserver_cluster.asg_name
+  scheduled_action_name  = "scale-out-during-business-hours"
+  min_size               = 2
+  max_size               = 10
+  desired_capacity       = 10
+  recurrence             = "0 9 * * *"
+
+}
+resource "aws_autoscaling_schedule" "scale_in_at_night" {
+  autoscaling_group_name = module.webserver_cluster.asg_name
+  scheduled_action_name  = "scale-in-at-night"
+  min_size               = 2
+  max_size               = 10
+  desired_capacity       = 2
+  recurrence             = "0 17 * * *"
+}
+
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket = "terraform-dani-bucket"
+    key    = "prod/services/webserver-cluster/terraform.tfstate"
+    region = "eu-west-1"
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
+}
